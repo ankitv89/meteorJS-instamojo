03813da2471812103f7ae513f18f9a3c5b9d67f8
Ashwini Chaudhary <monty.sinngh@gmail.com>
2016-07-25 18:53:53 +0530

    Let's no pass the headers each and very time, we can pass them once during the initialization.

--- instamojo.js
+++ instamojo.js
@@ -1,71 +1,56 @@
-// Write your package code here!
-
 // Variables exported by this module can be imported by other packages and
 // applications. See instamojo-tests.js for an example of importing.
 
 
 export const name = 'instamojo';
 
-Instamojo={
+Instamojo = function(api_key, auth_token, endpoint){
+    /*
+    * Get you `api_key` and `auth_token` from the https://www.instamojo.com/developers/ page.
+    * Here `endpoint` can be used to specify different API domain, for
+    * example: https://test.instamojo.com/api/1.1/
+    */
 
-  createRequest:function(payload,headers){
+    var endpoint = typeof endpoint !== 'undefined' ? endpoint : 'https://www.instamojo.com/api/1.1/';
+    var headers = {'X-Api-Key': api_key, 'X-Auth-Token': auth_token};
+    var sync_get = Meteor.wrapAsync(HTTP.get);
+    var sync_post = Meteor.wrapAsync(HTTP.post);
 
-    var sync = Meteor.wrapAsync(HTTP.post);
-    var result = sync('https://www.instamojo.com/api/1.1/payment-requests/',{params: payload,  headers: headers});
-    return result.data;
-
-
+    return  {
+    createRequest: function(payload){
+      var url = endpoint + 'payment-requests/';
+      var result = sync_post(url, {params: payload, headers: headers});
+      return result.data;
+    },
+    listRequest: function(){
+      var url = endpoint + 'payment-requests/';
+      var result = sync_get(url, {headers: headers});
+      return result.data;
+    },
+    getRequestDetails: function(id){
+      var url = endpoint + 'payment-requests/' + id + '/';
+      var result = sync_get(url, {headers: headers});
+      return result.data;
+    },
+    getPaymentDetails: function(payment_request_id, payment_id){
+      var url = endpoint + 'payment-requests/' + payment_request_id + '/' + payment_id + '/';
+      var result = sync_get(url, {headers: headers});
+      return result.data;
+    },
+    createRefund:function(payload){
+      var url = enpoint + 'refunds/';
+      var result = sync_post(url, {params: payload,  headers: headers});
+      return result.data;
+    },
+    listRefund:function(){
+      var url =  endpoint + 'refunds/';
+      var result = sync_get(url, {headers: headers});
+      return result.data;
+    },
+    getRefundDetails:function(id){
+      var url = endpoint + '/refunds/' + id + '/';
+      var result = sync_get(url, {headers: headers});
+      return result.data;
+    }
+  };
 }
-
-,
-  listRequest:function(headers){
-    var sync = Meteor.wrapAsync(HTTP.get);
-
-    var result = sync('https://www.instamojo.com/api/1.1/payment-requests/', {headers: headers});
-    return result.data;
-
-  },
-  getRequestDetails:function(headers,id){
-
-  var sync = Meteor.wrapAsync(HTTP.get);
-  var url='https://www.instamojo.com/api/1.1/payment-requests/'+id+'/';
-
-  var result=sync(url, {headers: headers});
-  return result.data;
-
-
-  },
-  getPaymentDetails:function(headers,payment_request_id,payment_id){
-var payload={};
-      var sync = Meteor.wrapAsync(HTTP.get);
-url='https://www.instamojo.com/api/1.1/payment-requests/'+payment_request_id+'/'+payment_id+'/';
-var result=sync(url,
-  {params: payload,  headers: headers});
-  return result.data;
-
-  },
-  createRefund:function(payload,headers){
-
-
-    var sync = Meteor.wrapAsync(HTTP.post);
-
-  var result=sync('https://www.instamojo.com/api/1.1/refunds/', {params: payload,  headers: headers});
-  return result.data;
-
-  },
-
-  listRefund:function(headers){
-var sync = Meteor.wrapAsync(HTTP.get);
-
-
-  var result=sync('https://www.instamojo.com/api/1.1/refunds/', {headers: headers});
-  return result.data;
-  },
-  getRefundDetails:function(headers,id){
-
-    var sync = Meteor.wrapAsync(HTTP.get);
-var url='https://www.instamojo.com/api/1.1/refunds/'+id+'/';
-  var result=sync(url, {headers: headers});
-  return result.data;
-  }
-};
